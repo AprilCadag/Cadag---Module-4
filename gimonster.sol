7f51c5b35f86a846d63efa81b679872988728d8e
AprilCadag <128223760+AprilCadag@users.noreply.github.com>
2024-05-17 00:23:00 +0800

    Update gimonster.sol

--- gimonster.sol
+++ gimonster.sol
@@ -1,1 +1,50 @@
-sdada
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract gimoGameToken is ERC20 {
+    address private gamer;
+
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+        gamer = msg.sender;
+    }
+
+    modifier onlyGamer() {
+        require(msg.sender == gamer, "Only owner can call this function");
+        _;
+    }
+
+    function mintGameToken(address account, uint256 amount) public onlyGamer {
+        _mint(account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function redeemGameToken(uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+    }
+
+    function checkBalance(address account) external view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function burnGameToken(uint256 amount) external {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+    }
+
+    function mintToMultipleAddresses(address[] memory recipients, uint256[] memory amounts) external onlyGamer {
+        require(recipients.length == amounts.length, "DegenToken: Array lengths mismatch");
+        for (uint256 i = 0; i < recipients.length; i++) {
+            _mint(recipients[i], amounts[i]);
+        }
+    }
+}
+
